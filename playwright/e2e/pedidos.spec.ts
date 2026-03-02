154eb65ee7d1badc056e55ba9d20cb9b12af29d4
KemerThyago <thyagokemer1@gmail.com>
2026-03-02 03:03:30 -0300

    Meu primeiro desafio

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -10,12 +10,21 @@ test('deve consultar um pedido aprovado', async ({ page }) => {
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
   
   //Act
-  await page.getByTestId('search-order-id').fill('VLO-RLJARM')
+//   await page.getByTestId('search-order-id').fill()
+  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill('VLO-RLJARM')
   await page.getByTestId('search-order-button').click()
 
   //Assert
-  await expect(page.getByTestId('order-result-id')).toBeVisible()
-  await expect(page.getByTestId('order-result-id')).toContainText('VLO-RLJARM')
-  await expect(page.getByTestId('order-result-status')).toBeVisible()
-  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
-})
+ 
+//   await expect(page.getByTestId('order-result-id')).toBeVisible({timeout: 30_000})
+//   await expect(page.getByTestId('order-result-id')).toContainText('VLO-RLJARM')
+  await expect(page.getByText('Pedido', { exact: true })).toBeVisible()
+  await expect(page.getByText('VLO-RLJARM')).toContainText('VLO-RLJARM')
+  
+//   await expect(page.getByTestId('order-result-status')).toBeVisible()
+//   await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
+//  await expect(page.getByText('APROVADO')).toBeVisible()
+
+ await expect(page.locator('.bg-green-100').filter({ hasText: 'APROVADO' })).toBeVisible()
+ await expect(page.getByText('APROVADO')).toContainText('APROVADO')
+})  
