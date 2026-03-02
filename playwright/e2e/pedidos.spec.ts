3786c52c38f8e1070e50d291bd5dabd879559ac0
KemerThyago <thyagokemer1@gmail.com>
2026-03-02 00:40:43 -0300

    Arrange, Act, Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,20 +1,21 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test'
+
+//AAA - Arrange, Act, Assert
 
 test('deve consultar um pedido aprovado', async ({ page }) => {
-  await page.goto('http://localhost:5173/');
-  //Checkpoint
-  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
-  //Ação de clicar no link de consulta de pedido
-  await page.getByRole('link', { name: 'Consultar Pedido' }).click();
-  //Checkpoint
-  await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
-  // Ação de preencher o campo de busca com o número do pedido
-  await page.getByTestId('search-order-id').fill('VLO-RLJARM');
-  await page.getByTestId('search-order-button').click();
-  //Checkpoint
-  await expect(page.getByTestId('order-result-id')).toBeVisible();
-  await expect(page.getByTestId('order-result-id')).toContainText('VLO-RLJARM');
+  //Arrange
+  await page.goto('http://localhost:5173/')
+  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click()  
+  await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
+  
+  //Act
+  await page.getByTestId('search-order-id').fill('VLO-RLJARM')
+  await page.getByTestId('search-order-button').click()
 
-  await expect(page.getByTestId('order-result-status')).toBeVisible();
-  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
-});
+  //Assert
+  await expect(page.getByTestId('order-result-id')).toBeVisible()
+  await expect(page.getByTestId('order-result-id')).toContainText('VLO-RLJARM')
+  await expect(page.getByTestId('order-result-status')).toBeVisible()
+  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
+})
