cb8767d1f273669f968b4191eef0a1f9027555e5
KemerThyago <thyagokemer1@gmail.com>
2026-05-17 23:13:01 -0300

    CT05 - desafio

--- playwright/e2e/checkout.spec.ts
+++ playwright/e2e/checkout.spec.ts
@@ -1,18 +1,16 @@
+import { deleteOrderByEmail } from '../support/database/orderRepositor'
 import { test, expect } from '../support/fixtures'
 
 test.describe('Checkout', () => {
 
 
-  test.beforeEach(async ({ page }) => {
-    await page.goto('/order')
-    await expect(page.getByRole('heading', { name: 'Finalizar Pedido' })).toBeVisible()
-  })
-
   test.describe('validações de campos obrigatórios', () => {
 
     let alerts: any
 
-    test.beforeEach(async ({ app }) => {
+    test.beforeEach(async ({ page, app }) => {
+      await page.goto('/order')
+      await expect(page.getByRole('heading', { name: 'Finalizar Pedido' })).toBeVisible()
       alerts = app.checkout.elements.alerts
     })
 
@@ -104,6 +102,45 @@ test.describe('Checkout', () => {
       await expect(alerts.terms).toHaveText('Aceite os termos')
     })
   })
+  test.describe('Pagamento e Confirmação', () => {
+
+
+    test('CT05 - deve criar pedido com pagamento à vista com sucesso', async ({ page, app }) => {
+      const customer = {
+        name: 'Carlos',
+        lastname: 'Silva',
+        email: 'carlos.silva@exemplo.com',
+        phone: '11987654321',
+        document: '529.982.247-25',
+        store: 'Velô Paulista',
+        total_price: 'R$ 40.000,00',
+        paymentMethod: 'À Vista',
+      }
+      await deleteOrderByEmail(customer.email)
+
+
+      await page.goto('/')
+      await page.getByRole('link', { name: 'Configure o Seu' }).click()
+      await expect(page).toHaveURL(/\/configure/)
+
+      // 2. Configurador -> Checkout
+      await app.configurator.expectPrice(customer.total_price)
+      await app.configurator.finishConfigurator()
+
+      // 3. Checkout
+      await app.checkout.expectLoaded()
+      await app.checkout.fillCustomerlData(customer)
+      await app.checkout.selectStore(customer.store)
+
+      await app.checkout.selectPaymentMethod(customer.paymentMethod)
+      await app.checkout.expectSummaryTotal(customer.total_price)
+
+      await app.checkout.acceptTerms()
+      await app.checkout.submit()
+
+      // 4. Confirmação
+      await app.checkout.expectResult('Pedido Aprovado!')
+    })
+
+  })
 })
-
-
